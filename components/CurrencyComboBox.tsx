da7dbf944f778cca7fe61e9608a28c6ae07f6190
schufot <156847189+schufot@users.noreply.github.com>
2024-08-08 12:05:13 +0200

    add preselected currency

--- components/CurrencyComboBox.tsx
+++ components/CurrencyComboBox.tsx
@@ -19,7 +19,9 @@ import {
 } from "@/components/ui/popover";
 import { useMediaQuery } from "@/hooks/use-media-query";
 import { Currencies, Currency } from "@/lib/currencies";
+import { UserSettings } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
+import SkeletonWrapper from "./SkeletonWrapper";
 
 export function CurrencyComboBox() {
   const [open, setOpen] = React.useState(false);
@@ -28,39 +30,57 @@ export function CurrencyComboBox() {
     null
   );
 
-  const userSettings = useQuery({
+  const userSettings = useQuery<UserSettings>({
     queryKey: ["userSettings"],
     queryFn: () => fetch("/api/user-settings").then((res) => res.json()),
   });
 
+  React.useEffect(() => {
+    if (!userSettings.data) return;
+    const userCurrency = Currencies.find(
+      (currency) => currency.value === userSettings.data.currency
+    );
+    if (userCurrency) setSelectedOption(userCurrency);
+  }, [userSettings.data]);
+
   if (isDesktop) {
     return (
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button variant="outline" className="w-full justify-start">
-            {selectedOption ? <>{selectedOption.label}</> : <>Set currency</>}
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-[200px] p-0" align="start">
-          <OptionList setOpen={setOpen} setSelectedOption={setSelectedOption} />
-        </PopoverContent>
-      </Popover>
+      <SkeletonWrapper isLoading={userSettings.isFetching}>
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button variant="outline" className="w-full justify-start">
+              {selectedOption ? <>{selectedOption.label}</> : <>Set currency</>}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-[200px] p-0" align="start">
+            <OptionList
+              setOpen={setOpen}
+              setSelectedOption={setSelectedOption}
+            />
+          </PopoverContent>
+        </Popover>
+      </SkeletonWrapper>
     );
   }
 
   return (
-    <Drawer open={open} onOpenChange={setOpen}>
-      <DrawerTrigger asChild>
-        <Button variant="outline" className="w-full justify-start">
-          {selectedOption ? <>{selectedOption.label}</> : <>Set currency</>}
-        </Button>
-      </DrawerTrigger>
-      <DrawerContent>
-        <div className="mt-4 border-t">
-          <OptionList setOpen={setOpen} setSelectedOption={setSelectedOption} />
-        </div>
-      </DrawerContent>
-    </Drawer>
+    <SkeletonWrapper isLoading={userSettings.isFetching}>
+      <Drawer open={open} onOpenChange={setOpen}>
+        <DrawerTrigger asChild>
+          <Button variant="outline" className="w-full justify-start">
+            {selectedOption ? <>{selectedOption.label}</> : <>Set currency</>}
+          </Button>
+        </DrawerTrigger>
+        <DrawerContent>
+          <div className="mt-4 border-t">
+            <OptionList
+              setOpen={setOpen}
+              setSelectedOption={setSelectedOption}
+            />
+          </div>
+        </DrawerContent>
+      </Drawer>
+    </SkeletonWrapper>
   );
 }
 
---
+++ components/SkeletonWrapper.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+import { Skeleton } from "./ui/skeleton";
+
+function SkeletonWrapper({
+  children,
+  isLoading,
+  fullWidth = true,
+}: {
+  children: ReactNode;
+  isLoading: boolean;
+  fullWidth?: boolean;
+}) {
+  if (!isLoading) return children;
+  return (
+    <Skeleton className={cn(fullWidth && "w-full")}>
+      <div className="opacity-0">{children}</div>
+    </Skeleton>
+  );
+}
+
+export default SkeletonWrapper;
