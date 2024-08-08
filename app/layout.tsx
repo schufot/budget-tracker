f616ac9da86694ef5c31940ebd479de3c7e59692
schufot <156847189+schufot@users.noreply.github.com>
2024-08-08 09:17:37 +0200

    initialize

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import RootProviders from "@/components/providers/RootProviders";
+import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Budget Tracker",
+  description: "Manage your income and outcome streams",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en" className="dark" style={{ colorScheme: "dark" }}>
+        <body className={inter.className}>
+          <RootProviders>{children}</RootProviders>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
